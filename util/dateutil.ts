4fa6fca946010e049b91d6f2a66f6d0804918080
Kalrnlo <62822174+kalrnlo@users.noreply.github.com>
2024-03-25 20:10:03 -0400

    Add rbxapi.ts

--- util/dateutil.ts
+++ util/dateutil.ts
@@ -7,44 +7,44 @@
 
 // ported from: https://github.com/bubshurb/libraries/blob/main/src/dateUtil/methods/daysInMonth.luau
 export function days_in_month(month: number, year: number) {
-        if (month === 4 || month === 6 || month === 9 || month === 11) {
-            return 30
-        } else if (month === 2) {
-            if (year % 4 === 0 && year % 100 !== 0) {
-                return 29
-            } else {
-                return 28
-            }
-        } else {
-            return 31
-        }
+	if (month === 4 || month === 6 || month === 9 || month === 11) {
+		return 30
+	} else if (month === 2) {
+		if (year % 4 === 0 && year % 100 !== 0) {
+			return 29
+		} else {
+			return 28
+		}
+	} else {
+		return 31
+	}
 }
 
 export function days_between_dates(date1: Date, date2 = new Date()) {
-    const y2 = date2.getUTCFullYear(),
-        m2 = date2.getUTCMonth(),
-        d2 = date2.getUTCDay();
-    let y1 = date1.getUTCFullYear(),
-        m1 = date1.getUTCMonth(),
-        d1 = date1.getUTCDay();
-    let days = 0
+	const y2 = date2.getUTCFullYear(),
+		m2 = date2.getUTCMonth(),
+		d2 = date2.getUTCDay();
+	let y1 = date1.getUTCFullYear(),
+		m1 = date1.getUTCMonth(),
+		d1 = date1.getUTCDay();
+	let days = 0
 
-    while (d1 < d2 || m1 < m2 || y1 < y1) {
-        const daysincurrentmonth = days_in_month(m1, y1)
-        d1++
+	while (d1 < d2 || m1 < m2 || y1 < y1) {
+		const daysincurrentmonth = days_in_month(m1, y1)
+		d1++
 
-        if (d1 > daysincurrentmonth) {
-            d1 = 1
-            m1++
+		if (d1 > daysincurrentmonth) {
+			d1 = 1
+			m1++
 
-            if (m1 > 12) {
-                m1 = 1
-                y1++
-            }
-        }
-        days++
-    }
-    return days
+			if (m1 > 12) {
+				m1 = 1
+				y1++
+			}
+		}
+		days++
+	}
+	return days
 }
 
 export function pack_date(date = new Date) {
@@ -52,5 +52,5 @@ export function pack_date(date = new Date) {
 }
 
 export function unpack_date(u32date: number) {
-    return new Date(u32date >> 9, (u32date >> 5) & 0xF, u24date & 0x1F)
+	return new Date(u32date >> 9, (u32date >> 5) & 0xF, u24date & 0x1F)
 }
---
+++ util/rbxapi.ts
@@ -0,0 +1,114 @@
+
+type rbxapiconfig = {
+	messaging_service_key: string,
+	group_api_key: string,
+	universe_id: number,
+}
+
+const base_url = "https://apis.roblox.com/"
+
+export class rbxapi {
+	static config = {
+		messaging_service_key = "",
+		group_api_key = "",
+		universe_id = 0,
+	} as rbxapiconfig
+
+	static setup(config: rbxapiconfig?) {
+		if (config !== null) {
+			this.config = config
+		}
+	}
+
+	static send_message(topic: string, message: string) {
+		if (message.length() > 1024) {
+			return Promise.reject("message cannot be longer than 1024 characters")
+		} else if (topic.length() > 80) {
+			return Promise.reject("topic cannot be longer than 80 characters")
+		}
+		let success = false
+
+		fetch(base_url + `messaging-service/v1/universes/${this.config.universe_id}/topics/${topic}`, {
+			headers = new Headers({
+				"x-api-key": this.config.messaging_service_key,
+				"Content-Type": "application/json",
+			}),
+			body = {
+				"message": message,
+			},
+			method = "POST",
+		}).then(function(responce) {
+			responce.json().then(function(jsonobj) {
+				success = jsonobj.body.keys().length === 0
+			}, (reason) => console.error)
+		}, (reason) => console.error)
+
+		if (success === true) {
+			return Promise.resolve("")
+		} else {
+			return Promise.reject("roblox api experienced an error")
+		}
+	}
+
+	static get_membership_info_for_users(group: number, userids: Array<number>, maxpagesize: number?, pagetoken: string?) {
+		const rank_sub_start_index = group.toString().length() + 13
+		let filter
+
+		if (userids.length > 1) {
+			filter = `filter="user in [`
+	
+			userids.forEach(function(value, index) {
+				if (index !== userids.length - 1) {
+					this += `'users/${value}', `
+				} else {
+					this += `'users/${value}']"`
+				}
+			}, filter)
+		} else if (userids.length === 1) {
+			filter = `filter="user == 'users/${userids[0] || 0}'" `
+		} else {
+			return Promise.reject("userids array was left empty")
+		}
+
+		let requesturl = base_url + `cloud/v2/groups/${group}/memberships?maxPageSize=${maxpagesize || 10}&filter=${filter}`
+
+		if (pagetoken !== null) {
+			requesturl + `&pageToken=${pagetoken}`
+		}
+		let membership_info_array = new Array(userids.length())
+		let next_page_token
+
+		fetch(requesturl, { headers = new Headers("x-api-key", this.config.group_api_key) }).then(function(responce) {
+			responce.json().then(function(jsonobj) {
+				next_page_token = jsonobj.nextPageToken
+				let index = 0
+
+				for ([key, value] in Object.entries(jsonobj.groupMemberships)) {
+					membership_info_array[index] = {
+						user_id = (value.user as string).substring(6),
+						rank = (value.role as string).substring(rank_sub_start_index)
+					}
+					
+					index++
+				}
+			}, (reason) => console.error)
+		}, (reason) => console.error)
+
+		if (nextpagetoken !== null) {
+			return Promise.resolve({
+				membership_info_array = membership_info_array as {
+					userid: string,
+					rank: string,
+				}[],
+				next_page_token = next_page_token as string,
+			})
+		} else {
+			return Promise.reject()
+		}
+	}
+
+	static get_thumbnails(userids: number[]) {
+
+	}
+}
+
