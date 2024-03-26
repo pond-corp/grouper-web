7bc82d0952622cfd242acc5ef0fba83de9a5684a
Kalrnlo <62822174+kalrnlo@users.noreply.github.com>
2024-03-25 21:35:15 -0400

    upd

--- util/rbxapi.ts
+++ util/rbxapi.ts
@@ -6,6 +6,25 @@
 
 const base_url = "https://apis.roblox.com/"
 
+type avatar_thumbnail_type = "avatar" | "avatar-bust" | "avatar-headshot"
+
+type avatar_headshot_sizes = "48x48" | "50x50" | "60x60" | "75x75" | "100x100" | "110x110" | "150x150" | "180x180" |
+	"352x352" | "420x420" | "720x720"
+
+type avatar_body_sizes = "30x30" | "48x48" | "60x60" | "75x75" | "100x100" | "110x110" | "140x140" | "150x150" |
+	"150x200" | "180x180" | "250x250" | "352x352" | "420x420" | "720x720"
+
+type avatar_bust_sizes = "48x48" | "50x50" | "60x60" | "75x75" | "100x100" | "150x150" | "180x180" | "352x352" |
+	"420x420"
+
+type avatar_thumbnail_size = avatar_headshot_sizes | avatar_body_sizes | avatar_bust_sizes
+
+type thumbnail_opts = {
+	format: "png" | "jpeg"?,
+	is_circular: boolean?,
+	max_retrys: number?,
+}
+
 export class rbxapi {
 	static config = {
 		messaging_service_key = "",
@@ -100,8 +119,97 @@ export class rbxapi {
 		}
 	}
 
-	static get_thumbnails(userids: number[]) {
+	static get_thumbnails(
+		type: avatar_thumbnail_type,
+		size: avatar_thumbnail_size,
+		user_ids: number[],
+		opts: thumbnail_opts?,
+	) {
+		const is_circular = opts.is_circular !== null ? opts.is_circular : false
+		const max_retrys = opts.max_retrys || 10
+		const format = opts.format || "jpeg"
 
+		fetch(
+			`https://thumbnails.roblox.com/v1/users/${type}?userIds=${user_ids.join(",")}&size=${size}&format=${format}&isCircular=${is_circular}`, {
+				"Content-Type": "application/json",
+			}
+		).then()
 	}
 }
 
+function getPlayerThumbnail (userIds, size, format = 'png', isCircular = false, cropType = 'body', retryCount = settings.maxRetries) {
+	// Validate userIds
+	if (Array.isArray(userIds)) {
+	  if (userIds.some(isNaN)) {
+		throw new Error('userIds must be a number or an array of numbers')
+	  }
+	  userIds = [...new Set(userIds)] // get rid of duplicates, endpoint response does this anyway
+	  if (userIds.length > 100) {
+		throw new Error(`too many userIds provided (${userIds.length}); maximum 100`)
+	  }
+	} else {
+	  if (isNaN(userIds)) {
+		throw new Error('userId is not a number')
+	  }
+	  userIds = [userIds]
+	}
+  
+	// Validate cropType
+	cropType = cropType.toLowerCase()
+	if (!Object.keys(eligibleSizes).includes(cropType)) {
+	  throw new Error(`Invalid cropping type provided: ${cropType} | Use: ${Object.keys(eligibleSizes).join(', ')}`)
+	}
+	const { sizes, endpoint } = eligibleSizes[cropType]
+  
+	// Validate size
+	size = size || sizes[sizes.length - 1]
+	if (typeof size === 'number') {
+	  size = `${size}x${size}`
+	}
+	if (!sizes.includes(size)) {
+	  throw new Error(`Invalid size parameter provided: ${size} | [${cropType.toUpperCase()}] Use: ${sizes.join(', ')}`)
+	}
+  
+	// Validate format
+	if (format.toLowerCase() !== 'png' && format.toLowerCase() !== 'jpeg') {
+	  throw new Error(`Invalid image type provided: ${format} | Use: png, jpeg`)
+	}
+  
+	return http({
+	  url: `https://thumbnails.roblox.com/v1/users/${endpoint}?userIds=${userIds.join(',')}&size=${size}&format=${format}&isCircular=${!!isCircular}`,
+	  options: {
+		resolveWithFullResponse: true,
+		followRedirect: true
+	  }
+	})
+	  .then(async ({ statusCode, body }) => {
+		let { data, errors } = JSON.parse(body)
+		if (statusCode === 200) {
+		  if (retryCount > 0) {
+			const pendingThumbnails = data.filter(obj => { return obj.state === 'Pending' }).map(obj => obj.targetId) // Get 'Pending' thumbnails as array of userIds
+			if (pendingThumbnails.length > 0) {
+			  await timeout(settings.retryDelay) // small delay helps cache populate on Roblox's end; default 500ms
+			  const updatedPending = await getPlayerThumbnail(pendingThumbnails, size, format, isCircular, cropType, --retryCount) // Recursively retry for # of maxRetries attempts; default 2
+			  data = data.map(obj => updatedPending.find(o => o.targetId === obj.targetId) || obj) // Update primary array's values
+			}
+		  }
+		  data = data.map(obj => {
+			if (obj.state !== 'Completed') {
+			  const settingsUrl = settings.failedUrl[obj.state.toLowerCase()] // user defined settings.json default image URL for blocked or pending thumbnails; default ""
+			  obj.imageUrl = settingsUrl || `https://noblox.js.org/moderatedThumbnails/moderatedThumbnail_${size}.png`
+			}
+			return obj
+		  })
+		  return data
+		} else if (statusCode === 400) {
+		  throw new Error(`Error Code ${errors.code}: ${errors.message} | endpoint: ${endpoint}, userIds: ${userIds.join(',')}, size: ${size}, isCircular: ${!!isCircular}`)
+		} else {
+		  throw new Error(`An unknown error occurred with getPlayerThumbnail() | endpoint: ${endpoint}, userIds: ${userIds.join(',')}, size: ${size}, isCircular: ${!!isCircular}`)
+		}
+	  })
+  }
+  
+  function timeout (ms) {
+	return new Promise(resolve => { setTimeout(resolve, ms) })
+  }
+  
