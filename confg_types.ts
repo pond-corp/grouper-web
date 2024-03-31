166e7de1f43043a4a2c2dcffef364d3d5491b783
Kalrnlo <62822174+kalrnlo@users.noreply.github.com>
2024-03-31 17:14:27 -0400

    Create confg_types.ts

---
+++ confg_types.ts
@@ -0,0 +1,41 @@
+import { tags } from "typia"
+
+export type color = string & tags.Pattern<"/\b(hex\(#?([0-9a-fA-F]{6})\))|hct\((\d+(?:\.\d+)?),\s*(\d+(?:\.\d+)?),\s*(\d+(?:\.\d+)?)\))\b/">;
+
+export interface config_template {
+	name: string;
+
+	event: {
+		auto_delete_events_after: string & tags.Format<"duration">;
+		minimum_high_rank: number & tags.Type<"uint32">;
+		minimum_rank: number & tags.Type<"uint32">;
+		check_if_events_are_full: boolean;
+		types: string[];
+	};
+	places: {
+		[place_name: string]: {
+			use_direct_join_urls: boolean;
+			universe_id: number;
+			place_id: number;
+		};
+	};
+	forms: {
+		maximum_paragraph_responce_length: number & tags.Type<"uint32">;
+		auto_delete_unreviewed_after: string & tags.Format<"duration">;
+		minimum_create_and_destroy_rank: number & tags.Type<"uint32">;
+		auto_delete_reviewed_after: string & tags.Format<"duration">;
+		minimum_review_rank: number & tags.Type<"uint32">;
+	};
+	socials: opt<{
+		guilded: opt<string & tags.Format<"url">>;
+		discord: opt<string & tags.Format<"url">>;
+	}>;
+	theme: color | {
+		primary: color;
+		secondary: color;
+		tertiary: color;
+		error: color;
+		neutral: color;
+		neutral_variant: color;
+	};
+}
